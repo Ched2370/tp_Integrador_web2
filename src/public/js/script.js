184c5e27bf7e0ec644785752adde4ff6ac039127
Ched2370 <mmibz.85@gmail.com>
2024-04-26 07:13:56 -0300

    guardando punto de control carrucel

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,13 +1,15 @@
-const div_container = document.getElementById('card');
-// consumimos Fake_API
-fetch('https://fakestoreapi.com/products')
-.then(res=>res.json())
-.then(json=>{
-    json.forEach(element => {
-        const img = document.createElement('img');
-        img.className = 'product';
-        img.src = element.image;
-        div_container.appendChild(img);
-    });
-});
+const div_container = document.getElementById("card");
 
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((json) => {
+    // Iteramos sobre los productos y los agregamos al contenedor
+    json.forEach((element) => {
+      const img = document.createElement("img");
+      img.className = "product";
+      img.src = element.image;
+      div_container.appendChild(img);
+    });})
+  .catch((err) => {
+    console.error("error al conectar la api", err);
+  });
